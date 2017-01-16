792f59f7becbe9357c51c13b986739d85b883847
Borja Minguela <bminguela001@ikasle.ehu.eus>
2017-01-16 14:37:46 +0100

    Añadido el logo en el login, y las clases para la comunicacion con el server

--- Eurocity/app/src/main/java/com/example/borja/eurocity/NuevoViaje.java
+++ Eurocity/app/src/main/java/com/example/borja/eurocity/NuevoViaje.java
@@ -3,16 +3,21 @@ package com.example.borja.eurocity;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.graphics.Color;
+import android.speech.tts.TextToSpeech;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.webkit.WebView;
 import android.widget.LinearLayout;
+import android.widget.Toast;
 
 import com.example.borja.eurocity.model.Viaje;
 
+import java.util.Locale;
+
 public class NuevoViaje extends AppCompatActivity {
 
+    private TextToSpeech tts;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -23,8 +28,24 @@ public class NuevoViaje extends AppCompatActivity {
         wView.setLayerType(WebView.LAYER_TYPE_SOFTWARE, null);
         wView.setInitialScale(1);
         wView.setVerticalScrollBarEnabled(false);
+
+        tts=new TextToSpeech(this,new TextToSpeech.OnInitListener() {
+            @Override
+            public void onInit(int status) {
+                if (status != TextToSpeech.ERROR) {
+                    tts.setLanguage(new Locale("spa", "ESP"));
+                }
+            }
+        });
         //LinearLayout layout = (LinearLayout) findViewById(R.id.activity_nuevo_viaje);
         //layout.addView(wView);
+    }
+
+    public void speak(View view){
+        String toSpeak = view.getTag().toString();
+        Toast.makeText(getApplicationContext(), toSpeak,Toast.LENGTH_SHORT).show();
+        tts.speak(toSpeak, TextToSpeech.QUEUE_FLUSH, null);
+
     }
 
     public void viajar(View view){
